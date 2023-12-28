b791b5e41c469fa408080131097e62e659de12cb
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2023-12-28 11:51:15 +0900

    py

--- authoring/jupyter_tools.mk
+++ authoring/jupyter_tools.mk
@@ -7,23 +7,23 @@ this_dir:=$(dir $(lastword $(MAKEFILE_LIST)))
 #
 # default source files
 #
-nb_srcs  ?= $(wildcard nb_src/source/*/*.{ml,c,py,sos})
-aux_srcs ?= $(wildcard nb_src/source/*/img/*.svg) $(wildcard nb_src/source/*/img/*.png)
+nb_srcs  ?= $(wildcard nb/source/*/*.{ml,c,py,sos})
+aux_srcs ?= $(wildcard nb/source/*/img/*.svg) $(wildcard nb/source/*/img/*.png)
 mk_nb_flags ?= 
 
 # users on behhalf of which we run jupyter servers
-users_csv ?= all_users.csv
+users_csv ?= users.csv
 users     ?= $(shell awk '{if(NR>1) print $$1}' FS=, $(users_csv))
 
 #
 # target files
 #
 ipynbs        :=
-ipynbs        += $(patsubst nb_src/source/%,notebooks/source/%.ipynb,$(nb_srcs))
+ipynbs        += $(patsubst nb/source/%,notebooks/source/%.ipynb,$(nb_srcs))
 ipynb_answers := 
-ipynb_answers += $(patsubst nb_src/source/%,notebooks/source/ans_%.ans.ipynb,$(nb_srcs))
-aux           := $(patsubst nb_src/source/%,notebooks/source/%,$(aux_srcs))
-aux_answers   := $(patsubst nb_src/source/%,notebooks/source/ans_%,$(aux_srcs))
+ipynb_answers += $(patsubst nb/source/%,notebooks/source/ans_%.ans.ipynb,$(nb_srcs))
+aux           := $(patsubst nb/source/%,notebooks/source/%,$(aux_srcs))
+aux_answers   := $(patsubst nb/source/%,notebooks/source/ans_%,$(aux_srcs))
 
 #
 # compile
@@ -32,18 +32,18 @@ compile : $(ipynbs) $(ipynb_answers) $(aux) $(aux_answers)
 prob : $(ipynbs) $(aux)
 ans : $(ipynb_answers) $(aux_answers)
 
-$(ipynbs) : notebooks/source/%.ipynb : nb_src/source/% notebooks/created
+$(ipynbs) : notebooks/source/%.ipynb : nb/source/% notebooks/created
 	mkdir -p $(dir $@)
 	$(this_dir)mk_nb.py $(mk_nb_flags) --output $@ $< 
 
-$(ipynb_answers) : notebooks/source/ans_%.ans.ipynb : nb_src/source/% notebooks/created
+$(ipynb_answers) : notebooks/source/ans_%.ans.ipynb : nb/source/% notebooks/created
 	mkdir -p $(dir $@)
 	$(this_dir)mk_nb.py $(mk_nb_flags) --output $@ --labels ans $< 
 
-$(aux) : notebooks/source/% : nb_src/source/% notebooks/created
+$(aux) : notebooks/source/% : nb/source/% notebooks/created
 	install --mode=0644 -D $< $@
 
-$(aux_answers) : notebooks/source/ans_% : nb_src/source/% notebooks/created
+$(aux_answers) : notebooks/source/ans_% : nb/source/% notebooks/created
 	install --mode=0644 -D $< $@
 
 notebooks/created :
@@ -95,5 +95,5 @@ nb : $(notebooks_dirs) $(nbgrader_configs)
 #
 feedback_class ?= pl
 feedback :
-	sqlite3 $(users_sqlite) 'select "user="||user||"   passwd="||password from a where class="$(feedback_class)"'
+	sqlite3 $(users_sqlite) 'select "access https://taulec.zapto.org:8000/ with user="||user||"   passwd="||password from a where class="$(feedback_class)"'
 
