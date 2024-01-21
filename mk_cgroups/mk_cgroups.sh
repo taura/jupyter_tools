e9b8336ef6360d16d4cb07de50e702b65b2fa95b
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2024-01-21 18:07:01 +0900

    mk_cgroups

--- mk_cgroups/mk_cgroups.sh
+++ mk_cgroups/mk_cgroups.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 root=/sys/fs/cgroup/taulec
-mkdir -p ${root}
+sudo mkdir -p ${root}
 echo +memory | sudo tee ${root}/cgroup.subtree_control
 
 # 
 for u in $(ls /home); do
-    mkdir -p ${root}/${u}
+    echo "=== ${u} ==="
+    sudo mkdir -p ${root}/${u}
     echo 256M | sudo tee ${root}/${u}/memory.high
 done
