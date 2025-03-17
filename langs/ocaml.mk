dc01288aa4df2bcb6b273cd1afdb0fb7f47642ac
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2025-03-17 11:14:59 +0900

    ocaml

--- langs/ocaml.mk
+++ langs/ocaml.mk
@@ -7,7 +7,7 @@ install:
 	eval $$(opam env) && opam install --yes jupyter menhir
 # jupyter-archimedes 
 	~/.opam/default/bin/ocaml-jupyter-opam-genspec
-	jupyter kernelspec install --user --name ocaml-jupyter ~/.opam/default/share/jupyter
+	. ~share/venv/jupyter/bin/activate && jupyter kernelspec install --user --name ocaml-jupyter ~/.opam/default/share/jupyter
 
 uninstall:
 	rm -rf ~/.opam
