e26ee82a94e27be089a0c12543d027f587d39e81
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2025-03-22 11:05:56 +0900

    ocaml

--- langs/ocaml.sh
+++ langs/ocaml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # must be executed on each host
 bash -c "sh <(curl -fsSL https://opam.ocaml.org/install.sh)"
-sudo apt install unzip bubblewrap
+sudo apt install unzip bubblewrap libgmp-dev libzmq3-dev pkg-config
