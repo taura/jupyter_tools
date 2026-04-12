95e49d61ce906b3561806c008cac03043b8a0541
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2026-04-12 23:24:04 +0900

    sync

--- sync_hist/sync_hist.sh
+++ sync_hist/sync_hist.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-src=/home/u26099
 dst=hist
 mkdir -p ${dst}
 
 shopt -s nullglob
-printf "%s\n" ${src}/notebooks/pl/*/hist.sqlite ${src}/notebooks/pl/*/problems/*/*/hist.sqlite | rsync -avR --files-from=- / "${dst}/"
+printf "%s\n" /home/*/notebooks/pl/*/hist.sqlite /home/*/notebooks/pl/*/problems/*/*/hist.sqlite | rsync -avR --files-from=- / "${dst}/"
 shopt -u nullglob
 
 sudo chown -R tau:tau ${dst}
