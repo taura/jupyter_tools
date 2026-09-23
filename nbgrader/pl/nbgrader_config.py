e94e1b7d8dfac09efea136e750d54a9b34173aad
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2026-09-23 09:47:52 +0900

    nbgrader_config.py

--- nbgrader/pl/nbgrader_config.py
+++ nbgrader/pl/nbgrader_config.py
@@ -7,7 +7,7 @@ c = get_config()  #noqa
 import os
 c.CourseDirectory.course_id = 'pl'
 c.CourseDirectory.root = os.path.expanduser('~/assignments')
-c.ClearSolutions.code_stub = dict(sos='') # python='', OCaml='', c='', bash='', 
+#c.ClearSolutions.code_stub = dict(sos='') # python='', OCaml='', c='', bash='', 
 
 ###############################
 
